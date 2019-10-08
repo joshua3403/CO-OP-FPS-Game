0e62e52483bca8baed52b68c1fa5bbbd5ae0962a
joshua3403 <joshua3403@gmail.com>
2019-10-08 16:50:45 +0900

    01. Create the player class

---
+++ COOPFPS/Intermediate/Build/Win64/COOPFPSEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -0,0 +1,1 @@
+#include "E:/Unreal4/CO-OP-FPS-Game/COOPFPS/Intermediate/Build/Win64/COOPFPSEditor/Development/Engine/SharedPCH.Engine.h"
---
+++ COOPFPS/Intermediate/Build/Win64/UE4Editor/Inc/COOPFPS/COOPFPSGameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef COOPFPS_COOPFPSGameModeBase_generated_h
+#error "COOPFPSGameModeBase.generated.h already included, missing '#pragma once' in COOPFPSGameModeBase.h"
+#endif
+#define COOPFPS_COOPFPSGameModeBase_generated_h
+
+#define COOPFPS_Source_COOPFPS_COOPFPSGameModeBase_h_15_RPC_WRAPPERS
+#define COOPFPS_Source_COOPFPS_COOPFPSGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define COOPFPS_Source_COOPFPS_COOPFPSGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesACOOPFPSGameModeBase(); \
+	friend struct Z_Construct_UClass_ACOOPFPSGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ACOOPFPSGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/COOPFPS"), NO_API) \
+	DECLARE_SERIALIZER(ACOOPFPSGameModeBase)
+
+
+#define COOPFPS_Source_COOPFPS_COOPFPSGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesACOOPFPSGameModeBase(); \
+	friend struct Z_Construct_UClass_ACOOPFPSGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ACOOPFPSGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/COOPFPS"), NO_API) \
+	DECLARE_SERIALIZER(ACOOPFPSGameModeBase)
+
+
+#define COOPFPS_Source_COOPFPS_COOPFPSGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ACOOPFPSGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACOOPFPSGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACOOPFPSGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACOOPFPSGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACOOPFPSGameModeBase(ACOOPFPSGameModeBase&&); \
+	NO_API ACOOPFPSGameModeBase(const ACOOPFPSGameModeBase&); \
+public:
+
+
+#define COOPFPS_Source_COOPFPS_COOPFPSGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ACOOPFPSGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACOOPFPSGameModeBase(ACOOPFPSGameModeBase&&); \
+	NO_API ACOOPFPSGameModeBase(const ACOOPFPSGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACOOPFPSGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACOOPFPSGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACOOPFPSGameModeBase)
+
+
+#define COOPFPS_Source_COOPFPS_COOPFPSGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define COOPFPS_Source_COOPFPS_COOPFPSGameModeBase_h_12_PROLOG
+#define COOPFPS_Source_COOPFPS_COOPFPSGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	COOPFPS_Source_COOPFPS_COOPFPSGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	COOPFPS_Source_COOPFPS_COOPFPSGameModeBase_h_15_RPC_WRAPPERS \
+	COOPFPS_Source_COOPFPS_COOPFPSGameModeBase_h_15_INCLASS \
+	COOPFPS_Source_COOPFPS_COOPFPSGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define COOPFPS_Source_COOPFPS_COOPFPSGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	COOPFPS_Source_COOPFPS_COOPFPSGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	COOPFPS_Source_COOPFPS_COOPFPSGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	COOPFPS_Source_COOPFPS_COOPFPSGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	COOPFPS_Source_COOPFPS_COOPFPSGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> COOPFPS_API UClass* StaticClass<class ACOOPFPSGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID COOPFPS_Source_COOPFPS_COOPFPSGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
