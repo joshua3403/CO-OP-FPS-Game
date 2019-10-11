b67ddb0f532eb390b5f580dea31a3dffae7852bb
joshua3403 <joshua3403@gmail.com>
2019-10-11 15:53:00 +0900

    07. Add Jump

--- COOPFPS/Source/COOPFPS/Private/SCharacter.cpp
+++ COOPFPS/Source/COOPFPS/Private/SCharacter.cpp
@@ -73,5 +73,8 @@ void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 	PlayerInputComponent->BindAction("Crouch", IE_Pressed, this, &ASCharacter::BeginCrouch);
 	PlayerInputComponent->BindAction("Crouch", IE_Released, this, &ASCharacter::EndCrouch);
 
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+
+
 }
 
